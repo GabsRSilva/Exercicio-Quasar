2aa5cd71f661cfbc0fd40b0ef9dc53d09070a4ec
ricardopereira <ricardompb@outlook.com>
2024-07-03 22:11:05 -0300

    ajustes

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,12 +2,6 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 import { Notify } from 'quasar'
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
 const api = axios.create({ baseURL: 'http://localhost:3000' })
 const http = axios.create()
 
@@ -18,7 +12,6 @@ api.interceptors.request.use(function (config) {
   return Promise.reject(error)
 })
 
-// Add a response interceptor
 api.interceptors.response.use(function (response) {
   return response
 }, function (error) {
@@ -27,21 +20,12 @@ api.interceptors.response.use(function (response) {
     type: 'negative',
     message
   })
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
   return Promise.reject(error)
 })
 
 export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
   app.config.globalProperties.$axios = axios
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
   app.config.globalProperties.$api = api
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
 })
 
 export { api, http }
